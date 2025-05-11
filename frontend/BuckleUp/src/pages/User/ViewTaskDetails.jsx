82818c6933e9b6654d4ec985761092e4879b4020
raghavragarwal <166992227+raghavragarwal@users.noreply.github.com>
2025-05-12 00:18:28 +0530

    Project First Usable Draft Done

--- frontend/BuckleUp/src/pages/User/ViewTaskDetails.jsx
+++ frontend/BuckleUp/src/pages/User/ViewTaskDetails.jsx
@@ -5,6 +5,7 @@ import { API_PATHS } from '../../utils/apiPaths';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import moment from 'moment';
 import AvatarGroup from '../../components/AvatarGroup';
+import { LuSquareArrowOutUpRight } from 'react-icons/lu';
 
 const ViewTaskDetails = () => {
   const { id } = useParams();
@@ -40,10 +41,35 @@ const ViewTaskDetails = () => {
   };
 
   // handle todo check
-  const updateTodoChecklist = async (index) => {};
+  const updateTodoChecklist = async (index) => {
+    const todoChecklist = [...task?.todoChecklist];
+    const taskId = id;
+
+    if (todoChecklist && todoChecklist[index]) {
+      todoChecklist[index].completed = !todoChecklist[index].completed;
+
+      try {
+        const response = await axiosInstance.put(
+          API_PATHS.TASKS.UPDATE_TASK_CHECKLIST(taskId),
+          { todoChecklist }
+        );
+        if (response.status === 200) {
+          setTask(response.data?.task || task);
+        } else {
+          // Optionally revert the toggle if the API call fails.
+          todoChecklist[index].completed = !todoChecklist[index].completed;
+        }
+      } catch (error) {
+        todoChecklist[index].completed = !todoChecklist[index].completed;
+      }
+    }
+  };
 
   // Handle attachment link lick
   const handleLinkClick = (link) => {
+    if (!/^https?:\/\//i.test(link)) {
+      link = "https://" + link; // Default to HTTPS
+    }
     window.open(link, "_blank");
   };
 
@@ -107,6 +133,38 @@ const ViewTaskDetails = () => {
                   />
                 </div>
               </div>
+
+              <div className="mt-2">
+                <label className="text-xs font-medium text-slate-500">
+                  Todo Checklist
+                </label>
+
+                {task?.todoChecklist?.map((item, index) => (
+                  <TodoCheckList
+                    key={`todo_${index}`}
+                    text={item.text}
+                    isChecked={item?.completed}
+                    onChange={() => updateTodoChecklist(index)}
+                  />
+                ))}
+              </div>
+
+              {task?.attachments?.length > 0 && (
+                <div className="mt-2">
+                  <label className="text-xs font-medium text-slate-500">
+                    Attachments
+                  </label>
+
+                  {task?.attachments?.map((link, index) => (
+                    <Attachment
+                      key={`link_${index}`}
+                      link={link}
+                      index={index}
+                      onClick={() => handleLinkClick(link)}
+                    />
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         )}
@@ -127,4 +185,38 @@ const InfoBox = ({label, value})=>{
       </p>
     </>
   );
+};
+
+const TodoCheckList = ({text, isChecked, onChange}) => {
+  return (
+    <div className="flex items-center gap-3 p-3">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={onChange}
+        className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none cursor-pointer"
+      />
+
+      <p className="text-[13px] text-gray-800">{text}</p>
+    </div>
+  )
+};
+
+const Attachment = ({link, index, onClick}) => {
+  return (
+    <div
+      className="flex justify-between bg-gray-50 border border-gray-100 px-3 py-2 rounded-md mb-3 mt-2 cursor-pointer"
+      onClick={onClick}
+    >
+      <div className="flex-1 flex items-center gap-3">
+        <span className="text-xs text-gray-400 font-semibold mr-2">
+          {index < 9 ? `0${index + 1}` : index + 1}
+        </span>
+
+        <p className="text-xs text-black">{link}</p>
+      </div>
+
+      <LuSquareArrowOutUpRight className="text-gray-400" />
+    </div>
+  )
 };
